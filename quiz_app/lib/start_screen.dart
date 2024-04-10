6d1a871b18aaa77b46339437832f09b4718eceab
BenGJ10 <bengj1015@gmail.com>
2024-04-10 17:20:15 +0530

    Worked till the gradient part

---
+++ quiz_app/lib/start_screen.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key}); //key forward it to super class
+  @override
+  Widget build(context) {
+    return const Center(child: Text('Start Screen!'));
+  }
+}
