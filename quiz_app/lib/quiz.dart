830800aacbb6154b6e5eacdd3bc7f149dd4a09b2
BenGJ10 <bengj1015@gmail.com>
2024-04-15 23:06:21 +0530

    Switches from Home screen to Questions

--- quiz_app/lib/quiz.dart
+++ quiz_app/lib/quiz.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:quiz_app/start_screen.dart';
+import 'package:quiz_app/questions_screen.dart';
 
 class Quiz extends StatefulWidget {
   const Quiz({super.key});
@@ -10,21 +11,36 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  var activeScreen = 'start-screen';
+
+  void switchScreen() {
+    setState(() {
+      // Flutter will re-execute the build method
+      activeScreen = 'questions-screen'; // Executes after StartScreen()
+    });
+  }
+
   @override
   Widget build(context) {
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
-            decoration: const BoxDecoration(
-                gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 69, 13, 167),
-                Color.fromARGB(255, 76, 4, 115)
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            )),
-            child: const StartScreen()),
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+            colors: [
+              Color.fromARGB(255, 69, 13, 167),
+              Color.fromARGB(255, 76, 4, 115)
+            ],
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+          )),
+          child: screenWidget,
+        ),
       ),
     );
   }
