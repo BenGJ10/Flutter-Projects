4e716edf4c48c8394160b9868a3c1feffae5a4e6
BenGJ10 <bengj1015@gmail.com>
2024-05-17 23:51:08 +0530

    Added Icons

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,8 +1,16 @@
 import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
 
 const uuid = Uuid(); // generate unique ids anywhere in the file
 
 enum Category { food, travel, leisure, study } // pre-defined allowed values
+
+const CategoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.study: Icons.school,
+};
 
 class Expense {
   Expense(
